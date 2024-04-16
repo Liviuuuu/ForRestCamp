b50ddd0f239d9684b3231b6528a8d0a9d775a686
Liviu Florea <florealiviu22@gmail.com>
2024-04-16 16:11:42 +0300

    connected mongoDB/mongoose

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/forrestcamp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const campground = new Campground({ title: "Craiova" });
+  await campground.save();
+  res.send(campground);
 });
 
 app.listen(3000, () => {
