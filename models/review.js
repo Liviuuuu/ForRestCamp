10ba2a76b6e8782c22449ceee41c31c30f5e6975
Liviu Florea <florealiviu22@gmail.com>
2024-05-14 13:26:15 +0300

    fixed delete Campground functionality

--- models/review.js
+++ models/review.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Campground = require("./campground");
+// const Campground = require("./campground");
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema(
