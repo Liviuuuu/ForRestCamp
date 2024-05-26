3eb64b311fa0c72150b66679b0565fdf802db7d3
Liviu Florea <florealiviu22@gmail.com>
2024-05-26 12:37:29 +0300

    implemented sort functionality and restructued code

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -10,67 +10,66 @@ const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const queryStr = req.query;
-  console.log("req.query:");
-  console.log(req.query);
-  if (req.query.search) {
+  const excludeFields = ["sort"];
+  let campgrounds;
+
+  if (queryStr.search) {
     console.log("Intrare 1");
-    console.log("req.query.campground(search):");
-    console.log(req.query.campground);
+
     const campgroundsList = await Campground.find({});
 
     const fuse = new Fuse(campgroundsList, {
-      minMatchCharLength: req.query.search.length,
+      minMatchCharLength: queryStr.search.length,
       ignoreLocation: true,
       keys: ["title", "location"],
     });
 
-    // const searchResults = fuse.search(req.query.search);
-    // const campgrounds = searchResults.map((searchResult) => searchResult.item);
-    const campgrounds = fuse.search(req.query.search).map((searchResult) => searchResult.item);
-
-    res.render("campgrounds/index", { campgrounds, queryStr });
-  } else if (Object.keys(req.query).length !== 0) {
+    campgrounds = fuse.search(queryStr.search).map((searchResult) => searchResult.item);
+  } else if (Object.keys(queryStr).length !== 0) {
     console.log("Intrare 2");
-    console.log("req.query:");
-    console.log(req.query);
-    
-    let queryObj = JSON.stringify(req.query);
-    //price
+    let queryObj = { ...queryStr };
+    excludeFields.forEach((elem) => {
+      delete queryObj[elem];
+    });
+
+    queryObj = JSON.stringify(queryObj);
+
+    // RegEx price
     queryObj = queryObj.replace(/\b(gte|lte)\b/g, (match) => `$${match}`);
-    //facilities
+    // RegEx facilities
     queryObj = queryObj.replace(
       /\b(parking|wifi|petFriendly|nonStopCheckIn|nonSmokingRooms|EVChargingStation|kitchen|barbecue|campfire|swimmingPool|hotTub|coffeTeaMaker)\b/g,
       (match) => `facilities.${match}`
     );
-    //roomFacilities
+    // RegEx roomFacilities
     queryObj = queryObj.replace(
       /\b(privateBathroom|refrigerator|TV|airConditioning|balcony|towels)\b/g,
       (match) => `roomFacilities.${match}`
     );
-    //funThingsToDo
+    // RegEx funThingsToDo
     queryObj = queryObj.replace(
       /\b(biking|ATV|motocross|fishing|canoeing|hiking|equestrian|climbing|rafting|walkingTours)\b/g,
       (match) => `funThingsToDo.${match}`
     );
 
     const queryString = JSON.parse(queryObj);
-    console.log(queryObj);
-    console.log(queryString);
-    // console.log(await Campground.find({});
-    // const campgrounds = await Campground.find({
-    //   "facilities.parking": req.query.campground.facilities.parking,
-    // });
-    const campgrounds = await Campground.find(queryString);
-    // const campgrounds = await Campground.find().where("parking").equals("true");
-    console.log(queryStr.propertyType !== undefined)
-    res.render("campgrounds/index", { campgrounds, queryStr });
+
+    let query = Campground.find(queryString);
+    
+    //SORTING LOGIC
+    if (queryStr.sort) {
+      const sortBy = queryStr.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    }
+
+    campgrounds = await query;
   } else {
     console.log("Intrare 3");
-    console.log(req.query);
-    // const queryStr = req.query;
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds, queryStr });
+
+    campgrounds = await Campground.find({});
   }
+
+  res.render("campgrounds/index", { campgrounds, queryStr });
 };
 
 module.exports.renderNewForm = (req, res) => {
